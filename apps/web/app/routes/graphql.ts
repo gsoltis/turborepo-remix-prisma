613f06774489f5e96c8c64e4af7a20bedb7f4e58
Greg Soltis <greg.soltis@vercel.com>
2022-02-08 16:20:41 -0800

    Catch errors on api route

--- apps/web/app/routes/graphql.ts
+++ apps/web/app/routes/graphql.ts
@@ -1,15 +1,19 @@
-import { graphql } from 'graphql'
-import { ActionFunction, json } from 'remix'
-import { schema, context } from 'server'
+import { graphql } from "graphql";
+import { ActionFunction, json } from "remix";
+import { schema, context } from "server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const body = await request.json()
-  const results = await graphql({
-    schema,
-    source: body.query,
-    variableValues: body.variables,
-    contextValue: context,
-  })
+  const body = await request.json();
+  try {
+    const results = await graphql({
+      schema,
+      source: body.query,
+      variableValues: body.variables,
+      contextValue: context,
+    });
 
-  return json(results)
-}
+    return json(results);
+  } catch (e: any) {
+    return e.toString();
+  }
+};
